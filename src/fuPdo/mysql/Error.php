e2393e6b51369a10e628b8c800283fa392d9414c
xiaofu <hongxinmou@fancydigital.com.cn>
2022-10-12 15:29:34 +0800

    add error

---
+++ src/fuPdo/mysql/Error.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace fuPdo\mysql;
+
+class Error
+{
+
+    public function __construct(\Exception $error)
+    {
+        $this->error_code = $error->getCode();
+        $this->error_message= $error->getMessage();
+    }
+
+    /**
+     * @var int
+     */
+    private $error_code = 0;
+
+    /**
+     * @return int
+     */
+    public function getErrorCode()
+    {
+        return $this->error_code;
+    }
+
+    /**
+     * @param int $error_code
+     */
+    public function setErrorCode($error_code)
+    {
+        $this->error_code = $error_code;
+    }
+
+    /**
+     * @var string
+     */
+    private $error_message = '';
+
+    /**
+     * @return string
+     */
+    public function getErrorMessage()
+    {
+        return $this->error_message;
+    }
+
+    /**
+     * @param string $error_message
+     */
+    public function setErrorMessage($error_message)
+    {
+        $this->error_message = $error_message;
+    }
+
+}
